18916af243b0142b5e4dcfd268e4c341dd727361
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2025-02-26 20:40:42 -0300

    aula introdução componente

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Constants from 'expo-constants';
+
+import Exemplo1 from './src/exemplos/ex-01'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Exemplo1 />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Constants.statusBarHeight || 12,
   },
 });
---
+++ src/exemplos/ex-01/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Exemplo1 () {
+  return(
+    <View style={styles.container}>
+      <Text>Exemplo 1</Text>
+    </View>
+  )
+}
+
+export default Exemplo1;
