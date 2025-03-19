08721bf0611e2ae971865a09ce77caed488ba1f6
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2025-03-19 19:18:45 -0300

    substituição

--- src/exemplos/ex-02/index.js
+++ src/exemplos/ex-02/index.js
@@ -1,18 +1,20 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image} from 'react-native';
 
 import styles from './styles';
+
 import img from '../../../assets/002-1-react-native.png';
-import Mensagem from '../mensagem';
+import Mensagem from './mensagem';
 
 function Exemplo2 () {
-  return(
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Atividade 1</Text>
-      <Image source={img} style={styles.imagem} />
-
-      <Mensagem />
-    </View>
-  )
+    return(
+        <View style={styles.container}>
+                <Text style={styles.titulo}>Exemplo 2</Text>
+                <Image source={img} style={styles.imagem} />
+                <Mensagem titulo={'ERRO'} mensagem={'você não clicou corretamente!'}/>    
+                <Mensagem titulo={'SUCESSO'} mensagem={'Acesso permitido'}/>    
+                <Mensagem titulo={'AVISO'} mensagem={'o tempo acabou'}/>    
+        </View>
+    );
 }
 
 export default Exemplo2;
