7060e696bd975dcfbd6b5c01a50661ebed4fc0ea
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2025-04-02 20:37:50 -0300

    exemplo 3

--- src/exemplos/ex-03/index.js
+++ src/exemplos/ex-03/index.js
@@ -1,20 +1,44 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Alert, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-function Exemplo3 () {
-  return(
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Exemplo 3</Text>
-      <Text style={styles.texto}>Variáveis vs State</Text>
-      <Button
-      onPress={() => {}}
-      title="Alerta"
-      color="deepskyblue"
-      accessibilityLabel="Mensagem de alerta"
-      />
-    </View>
-  )
+function Exemplo3() {
+
+    function handleExibeMensagem () {
+        Alert.alert('Teste')
+    }
+
+    const handleOutroJeitoFuncao = () => {
+        Alert.alert('Título', 'Texto mensagem', [
+            {
+              text: 'Meu texto',
+              onPress: () => Alert.alert('Tchau'),
+              style: 'default',
+            },
+          ])
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Exemplo 3</Text>
+            <Text style={styles.texto}>Variáveis vs State</Text>
+            <View style={styles.botaoAlert}>
+            <Button
+                onPress={handleOutroJeitoFuncao}
+                title="Alerta"
+                color="deepskyblue"
+                accessibilityLabel="Mensagem de alerta"
+            />
+            </View>
+
+            <View style={styles.container}>
+            <Text style={styles.texto}>Valor</Text>
+            <TouchableOpacity style={styles.opacityBotao}>
+              <Text>Adicionar +1</Text>
+            </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 export default Exemplo3;
--- src/exemplos/ex-03/styles.js
+++ src/exemplos/ex-03/styles.js
@@ -1,32 +1,49 @@
-import { StyleSheet } from "react-native";
-import { RFPercentage } from 'react-native-responsive-fontsize'
+import { StyleSheet } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    padding: 8,
-    flex: 1,
-    alignItems: 'center',
-    borderRadius: 20,
-  },
-  titulo: {
-      fontSize: RFPercentage(3),
-      fontWeight: 'bold',
-      borderWidth: 2,
-      borderColor: 'deepskyblue',
-      marginTop: 10,
-      marginBottom: 10,
-      padding: 8,
-      width: '100%',
-      height: 80,
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      borderRadius: 20,
-  },
-  texto: {
-    fontSize: RFPercentage(2.2),
-    color: 'deepskyblue',
-  }
+    container: {
+        backgroundColor: '#fff',
+        padding: 8,
+        flex: 1,
+        alignItems: 'center',
+        borderRadius: 20,
+    },
+    titulo: {
+        fontSize: RFPercentage(3),
+        color: 'deepskyblue',
+        fontWeight: 'bold',
+        borderWidth: 2,
+        borderColor: 'deepskyblue',
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 8,
+        width: '100%',
+        height: 80,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        borderRadius: 20,
+    },
+    texto: {
+        fontSize: RFPercentage(2.2),
+    },
+    botaoAlert: {
+        width: '100%',
+        margin: RFPercentage(1),
+    },
+    opacityBotao: {
+        width: '70%',
+        margin: RFPercentage(1),
+        backgroundColor: 'deepskyblue',
+        borderRadius: RFPercentage(2),
+        padding: RFPercentage(1),
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textoValor: {
+        fontSize: RFPercentage(2),
+        color: 'grey',
+    }
 });
 
 export default styles;
