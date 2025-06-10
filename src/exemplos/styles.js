5854383fcc904f3312c813e61805250b4c2d011d
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2025-06-10 20:40:34 -0300

    Update styles.js

--- src/exemplos/styles.js
+++ src/exemplos/styles.js
@@ -4,11 +4,14 @@ import { RFValue } from "react-native-responsive-fontsize";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    align
   },
   titulo: {
     fontSize: RFValue(20),
     fontWeight: 'bold',
     color: 'deepskyblue',
+    marginVertical: RFValue(10),
+    marginBottom: RFValue(20),
   },
   botao: {
     borderWidth: RFValue(1),
@@ -16,6 +19,9 @@ const styles = StyleSheet.create({
     borderRadius : RFValue(2),
     padding: RFValue(1),
     width: '80%',
+    alignItems: 'center',
+    padding: RFValue(5),
+
   },
  });
 
